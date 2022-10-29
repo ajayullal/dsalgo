8ff37d2c2ecdbb17156df8dee13233a13fcdb138
Ajay Ullal J K <ajayullal91@gmail.com>
2022-10-29 14:38:46 +0530

    Updated solution to Leetcode 53 - Max subarray

--- arrays/53-max-subarray.js
+++ arrays/53-max-subarray.js
@@ -2,15 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function (nums) {
-  let max = nums[0];
-  let currentSum = nums[0];
-  nums.slice(1).forEach((num) => {
-    if (max === undefined) max = num;
-    if (currentSum === undefined) currentSum = num;
-    if (currentSum < 0) currentSum = num;
-    else currentSum += num;
-    if (currentSum > max) max = currentSum;
-  });
-  return max;
+ var maxSubArray = function(nums) {
+  let max = nums[0], currentSum = nums[0];
+  
+  for(let i=1; i< nums.length; i++){
+      if(currentSum < 0) currentSum = nums[i];
+      else currentSum += nums[i];
+      if(currentSum > max) max = currentSum;
+  }
+  
+ return max;
 };
